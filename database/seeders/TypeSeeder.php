c1a8dbaf153517fd2809e6df2abea84ef22083ff
Shamble94 <fratus.nicholas@gmail.com>
2024-02-27 17:22:55 +0100

    Creati seeder migration model della tabella type

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+for($i=0; $i<10; $i++){
+    $type = new Type();
+
+}
+}
